cac4c7577d483c6ecf98573be410424c3fd08220
Aditya Agrawal <adityacosmos24@gmail.com>
2025-08-22 22:21:47 +0530

    fixing cors

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL, // ✅ use import.meta.env
   withCredentials: true,
 });
