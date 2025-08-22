585d94276c4e91421d7d913e8a37d9fb0ae632ed
Aditya Agrawal <adityacosmos24@gmail.com>
2025-08-22 22:04:37 +0530

    sfa

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL || "http://localhost:5000/api",
+  baseURL: process.env.BACKEND_URL,
   withCredentials: true,
 });
